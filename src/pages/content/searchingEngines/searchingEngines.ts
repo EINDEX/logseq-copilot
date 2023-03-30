4f225ff0f833ab13517f6eec956bb82fb46a6e67
EINDEX <snowstarlbk@gmail.com>
2023-03-30 13:56:04 +0800

    refactor: update search engine logic

--- src/pages/content/searchingEngines/searchingEngines.ts
+++ src/pages/content/searchingEngines/searchingEngines.ts
@@ -1,10 +1,19 @@
-interface SearchingEngine {
-  isMatch(): boolean;
-  getQuery(): string | null;
-  gotElement(): Element;
+abstract class SearchingEngine {
+  isMatch(): boolean {
+    return false;
+  }
+  getQuery(): string | null {
+    return null
+  }
+  gotElement(): Element | null {
+    return null
+  }
+  reload(callback: Function): null {
+    return null
+  }
 }
 
-export class Google implements SearchingEngine {
+export class Google extends SearchingEngine {
   // .google.com .google.ad .google.ae .google.com.af .google.com.ag .google.com.ai .google.al .google.am .google.co.ao .google.com.ar .google.as .google.at .google.com.au .google.az .google.ba .google.com.bd .google.be .google.bf .google.bg .google.com.bh .google.bi .google.bj .google.com.bn .google.com.bo .google.com.br .google.bs .google.bt .google.co.bw .google.by .google.com.bz .google.ca .google.cd .google.cf .google.cg .google.ch .google.ci .google.co.ck .google.cl .google.cm .google.cn .google.com.co .google.co.cr .google.com.cu .google.cv .google.com.cy .google.cz .google.de .google.dj .google.dk .google.dm .google.com.do .google.dz .google.com.ec .google.ee .google.com.eg .google.es .google.com.et .google.fi .google.com.fj .google.fm .google.fr .google.ga .google.ge .google.gg .google.com.gh .google.com.gi .google.gl .google.gm .google.gr .google.com.gt .google.gy .google.com.hk .google.hn .google.hr .google.ht .google.hu .google.co.id .google.ie .google.co.il .google.im .google.co.in .google.iq .google.is .google.it .google.je .google.com.jm .google.jo .google.co.jp .google.co.ke .google.com.kh .google.ki .google.kg .google.co.kr .google.com.kw .google.kz .google.la .google.com.lb .google.li .google.lk .google.co.ls .google.lt .google.lu .google.lv .google.com.ly .google.co.ma .google.md .google.me .google.mg .google.mk .google.ml .google.com.mm .google.mn .google.ms .google.com.mt .google.mu .google.mv .google.mw .google.com.mx .google.com.my .google.co.mz .google.com.na .google.com.ng .google.com.ni .google.ne .google.nl .google.no .google.com.np .google.nr .google.nu .google.co.nz .google.com.om .google.com.pa .google.com.pe .google.com.pg .google.com.ph .google.com.pk .google.pl .google.pn .google.com.pr .google.ps .google.pt .google.com.py .google.com.qa .google.ro .google.ru .google.rw .google.com.sa .google.com.sb .google.sc .google.se .google.com.sg .google.sh .google.si .google.sk .google.com.sl .google.sn .google.so .google.sm .google.sr .google.st .google.com.sv .google.td .google.tg .google.co.th .google.com.tj .google.tl .google.tm .google.tn .google.to .google.com.tr .google.tt .google.com.tw .google.co.tz .google.com.ua .google.co.ug .google.co.uk .google.com.uy .google.co.uz .google.com.vc .google.co.ve .google.vg .google.co.vi .google.com.vn .google.vu .google.ws .google.rs .google.co.za .google.co.zm .google.co.zw .google.cat
   isMatch(): boolean {
     const match = window.location.hostname.match(
@@ -36,7 +45,7 @@ export class Google implements SearchingEngine {
   }
 }
 
-export class Bing implements SearchingEngine {
+export class Bing extends SearchingEngine {
   isMatch(): boolean {
     const match = window.location.hostname.match(/bing(\.com)?(\.\w{2})?$/g);
     return !!match;
@@ -57,7 +66,7 @@ export class Bing implements SearchingEngine {
   }
 }
 
-export class DuckDuckGo implements SearchingEngine {
+export class DuckDuckGo extends SearchingEngine {
   isMatch(): boolean {
     const match = window.location.hostname.match(/duckduckgo\.com$/g);
     return !!match;
@@ -78,7 +87,7 @@ export class DuckDuckGo implements SearchingEngine {
   }
 }
 
-export class Yandex implements SearchingEngine {
+export class Yandex extends SearchingEngine {
   isMatch(): boolean {
     const match = window.location.hostname.match(/yandex\.[com|ru]$/g);
     return !!match;
@@ -101,7 +110,7 @@ export class Yandex implements SearchingEngine {
   }
 }
 
-export class SearX implements SearchingEngine {
+export class SearX extends SearchingEngine {
   isMatch(): boolean {
     // Check if the website is using the searx engine
     const meta = document.querySelector('head > meta[name="generator"]');
@@ -137,7 +146,7 @@ export class SearX implements SearchingEngine {
   }
 }
 
-export class Baidu implements SearchingEngine {
+export class Baidu extends SearchingEngine {
   isMatch(): boolean {
     // Check if the website is using the searx engine
     const match = !!window.location.hostname.match('baidu.com');
@@ -158,7 +167,7 @@ export class Baidu implements SearchingEngine {
     return container;
   }
 
-  reload(callback: Function) {
+  reload(callback: Function): null{
     const targetNode = document.getElementById('wrapper_wrapper')!;
     const observer = new MutationObserver(function (records) {
       for (const record of records) {
@@ -166,17 +175,18 @@ export class Baidu implements SearchingEngine {
           for (const node of record.addedNodes) {
             if ('id' in node && node.id === 'container') {
               callback();
-              return;
+              return null;
             }
           }
         }
       }
     });
     observer.observe(targetNode, { childList: true });
+    return null
   }
 }
 
-export class Kagi implements SearchingEngine {
+export class Kagi extends SearchingEngine {
   
   isMatch(): boolean {
     const match = window.location.hostname.match(/kagi\.com$/g);
