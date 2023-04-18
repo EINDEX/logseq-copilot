19dafe8d75c0dcc111e5977f93aa1a5ed032b4bc
EINDEX <snowstarlbk@gmail.com>
2023-04-19 00:10:35 +0800

    ci: fix pipeline

--- release.config.js
+++ release.config.js
@@ -8,7 +8,19 @@ module.exports = {
     ],
     '@semantic-release/release-notes-generator',
     "@semantic-release/changelog",
-    "@semantic-release/git",
+    ["@semantic-release/git", {
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }],
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          {path:'build/chrome-*.zip', label: "Chrome version"}, 
+          {path:'build/edge-*.zip', label: "Edge version"},
+          {path:'build/firefox-*.zip', label: "Firefox version"}
+      ],
+      },
+    ],
   ],
   branches: ['release'],
 };
